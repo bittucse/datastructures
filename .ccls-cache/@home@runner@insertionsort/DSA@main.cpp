ed557ca9ca7f75bacf7ad9bb2090664f4625c870
bittucse <31320420-bittucse@users.noreply.replit.com>
2024-01-17 02:42:39 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@insertionsort/DSA@main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void insertionSort(int arr[], int n)
+{
+   for(int i=0;i<n;i++){
+        int j=i;
+        while(j>0 && arr[j]<arr[j-1]){
+            int temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+            j--;
+        }
+
+   }
+}
+
+int main() {
+  std::cout << "Hello World!\n";
+}
